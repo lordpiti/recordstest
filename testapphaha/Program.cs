53da429d08e6842366ec888c78ccde03f913c7f4
Pablo Amil <eltiodelassortijas@gmail.com>
2017-04-05 01:15:47 +0100

    final tweaks

--- testapphaha/Program.cs
+++ testapphaha/Program.cs
@@ -10,41 +10,52 @@ namespace Records.App
     {
         static void Main(string[] args)
         {
-            if (args.Count() == 2)
+            try
             {
-                Console.WriteLine("Enter provider");
-                var partner = Console.ReadLine();
-                Console.WriteLine("Enter date");
-                var dateToCheck = Console.ReadLine();
-                //args = new string[] { @"C:\testapphaha\testapphaha\InputFiles\input1.txt", @"C:\testapphaha\testapphaha\InputFiles\input2.txt" };
-
-                var file1Path = args[0];
-                var file2Path = args[1];
-
-                string[] linesRecords = File.ReadAllLines(file1Path, Encoding.UTF8);
-
-                string[] linesLicenses = File.ReadAllLines(file2Path, Encoding.UTF8);
-
-                //Ignore the first three lines of each file, since it's for headers
-                var relevantLinesRecords = linesRecords.Skip(3);
-                var relevantLinesLicenses = linesLicenses.Skip(3);
-
-                //Create manually the instance of the object to use
-                //Since it's very simple for this small app I don't use an Ioc container
-                var recordLicenser = new RecordLicenser(relevantLinesRecords, relevantLinesLicenses);
-
-                var validRecords = recordLicenser.getValidRecords(partner, dateToCheck);
-
-                Console.WriteLine("Artist|Title|Usages|StartDate|EndDate");
-                foreach (var item in validRecords)
+                if (args.Count() == 2)
                 {
-                    Console.WriteLine(item);
+                    Console.WriteLine("Enter provider and date with the format: <partner name> <date in 'd MMM yyyy' or 'd MMMM yyyy'>");
+
+                    //args = new string[] { @"C:\testapphaha\testapphaha\InputFiles\input1.txt", @"C:\testapphaha\testapphaha\InputFiles\input2.txt" };
+
+                    var partnerAndDate = Console.ReadLine();
+
+                    var index = partnerAndDate.IndexOf(" ");
+                    var partner = partnerAndDate.Substring(0, index);
+                    var dateToCheck = partnerAndDate.Substring(index + 1, partnerAndDate.Length - index - 1);
+
+                    var file1Path = args[0];
+                    var file2Path = args[1];
+
+                    string[] linesRecords = File.ReadAllLines(file1Path, Encoding.UTF8);
+
+                    string[] linesLicenses = File.ReadAllLines(file2Path, Encoding.UTF8);
+
+                    //Ignore the first three lines of each file, since it's for headers
+                    var relevantLinesRecords = linesRecords.Skip(3);
+                    var relevantLinesLicenses = linesLicenses.Skip(3);
+
+                    //Create manually the instance of the object to use
+                    //Since it's very simple for this small app I don't use an Ioc container
+                    var recordLicenser = new RecordLicenser(relevantLinesRecords, relevantLinesLicenses);
+
+                    var validRecords = recordLicenser.getValidRecords(partner, dateToCheck);
+
+                    Console.WriteLine("Artist|Title|Usages|StartDate|EndDate");
+                    foreach (var item in validRecords)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.ReadLine();
                 }
-                Console.ReadLine();
+                else
+                {
+                    Console.WriteLine("syntax is '<exe file> <input file for the Music Contracts> <input file for the Distribution Partner Contracts>'");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("syntax is '<exe file> <inputfile1> <inputfile2>'");
+                Console.WriteLine("An error occurred. Probably you haven't specified the data in the right format.");
             }
         }
     }
